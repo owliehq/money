c246504a7e8ce6609736bb66979d510d11fd22a0
Xavier HEN <uminily@gmail.com>
2021-01-25 17:55:17 +0100

    fix: floating centims

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ export * from './InvoiceCalculator'
  * @param withVat
  * @param vat
  */
-export const convertToWithoutVAT = (withVat: number, vat: number): number => {
-  return Math.floor(withVat / (1 + vat))
+export const convertToWithoutVAT = (withVat: number, vat: number, floating = false): number => {
+  const beforeFloating = withVat / (1 + vat)
+
+  return !floating ? Math.floor(beforeFloating) : beforeFloating
 }
--- test/convert.test.ts
+++ test/convert.test.ts
@@ -1,4 +1,8 @@
-import { convertToWithoutVAT } from '../src'
+import { convertToWithoutVAT, InvoiceCalculator } from '../src'
+
+function getRandomCentims() {
+  return Math.floor(Math.random() * 65565) + 1
+}
 
 describe('Convert', () => {
   it('should convert withVAT amount to withoutVAT amount', () => {
@@ -8,5 +12,22 @@ describe('Convert', () => {
     const withoutVAT = convertToWithoutVAT(amount, vat)
 
     expect(withoutVAT).toBe(1895)
+  })
+
+  it('should convert withVAT amounts to withoutVAT amounts then reconvert it and find same values', () => {
+    const randomValues = Array(40000)
+      .fill(0)
+      .map((value, index) => index + 20)
+
+    const amounts = [250, 750, 550, 799, 1054, 689, 100, 101, 105, ...randomValues]
+    const vat = 0.055
+
+    const withoutVATs = amounts.map((amount) => convertToWithoutVAT(amount, vat, true))
+
+    const withVATs = withoutVATs.map((amount) => InvoiceCalculator.getWithVAT(amount, vat))
+
+    amounts.forEach((amount, index) => {
+      expect(amount).toBe(withVATs[index])
+    })
   })
 })
